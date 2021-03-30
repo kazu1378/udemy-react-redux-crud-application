e7aaf99b8f773befcaba927f2edf34eecff5fbae
kazu1378 <kazua0001@gmail.com>
2021-03-31 05:04:37 +0900

    impre del

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -13,8 +13,16 @@ class EventsIndex extends Component {
  renderEvents(){
    return _.map(this.props.events, event => (
      <tr key={event.id}>
-      <td>{event.id}</td>
-      <td>{event.title}</td>
+      <td>
+        <Link to = {`/events/${event.id}`}>
+          {event.id}
+        </Link>
+      </td>
+      <td>
+        <Link to = {`/events/${event.id}`}>
+          {event.title}
+        </Link>
+      </td>
       <td>{event.body}</td>
      </tr>
    ))
